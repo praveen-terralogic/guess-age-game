8112b6271dd4dd1fbf56b1a91d5a300c2ff6b906
Praveen Pal <praveen.pal@terralogic.com>
2022-05-05 13:12:44 +0530

    adeed samicolons

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let guessAge = parseInt(prompt("Guess the Real Age"));
 
 function checkAge(){
     if(realAge == guessAge){
-      alert("Great Job, you guess it right")
+      alert("Great Job, you guess it right");
         return;
     }
     (realAge > guessAge) ? alert("Enter a higer age") : alert("Enter a lower age");
     guessAge = parseInt(prompt("Guess the Real Age"));
-    checkAge()
+    checkAge();
 }
 
 checkAge();
